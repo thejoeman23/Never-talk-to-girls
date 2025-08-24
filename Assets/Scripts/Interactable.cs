2f8070f5603fc1d084b3d878b14bfc22bf2b3bd9
Jonathan Strong <epicjonathan24@gmail.com>
2025-08-24 17:48:00 -0400

    Squashed commit of the following:
    commit 85681f8acbca80a833a2f423c769f2c75b8c6f8e
    Author: Jonathan Strong <epicjonathan24@gmail.com>
    Date:   Sun Aug 24 17:47:36 2025 -0400
    
        Interact Button Visuals
    
        - Press E to Interact button shows above the nearest thing to interact with.
    
    commit 7d70003ba9ef8625464c33a01323cd0e4ea81b9f
    Author: Jonathan Strong <epicjonathan24@gmail.com>
    Date:   Sun Aug 24 15:03:13 2025 -0400
    
        Base Setup
    
        - Added Interactor and Interactable
        - Groundwork for UIManager which will show the interact button

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))] // If there is no collider it cant be seen by the Interactor
+public class Interactable : MonoBehaviour, IInteractable // <- See how it derives?
+{
+    // An event called on interacting with this object
+    [SerializeField] public UnityEvent _onInteract;
+    [SerializeField] public bool _canInteract = true;
+
+    public void Start()
+    {
+        gameObject.tag = "Interactable";
+    }
+    
+    // Call the function
+    public void Interact()
+    {
+        // If _canInteract is false then cut the function short and dont run the rest of the code
+        if (!_canInteract)
+            return;
+        
+        Debug.Log("Interact Worked");
+        
+        _onInteract.Invoke();
+    }
+}
+
+// An interface is a list of functions that whatever derives from it must contain
+// Deriving is a way of inheriting variables and functions from another script, or in this case, interface
+public interface IInteractable
+{
+    public void Interact();
+}
